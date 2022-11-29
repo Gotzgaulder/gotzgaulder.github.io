01c2b4d50ea29818237bf8d120e43e3b2507087e
Gotzgaulder <98315503+Gotzgaulder@users.noreply.github.com>
2022-11-29 19:43:55 +0300

    Delete Norm.js

--- Norm.js
+++
@@ -1,9 +0,0 @@
-function darkmode() {
-  const body = document.body
-  const wasDarkMode = localStorage.getItem('darkmode') === 'true'
-
-  localStorage.setItem('darkmode', !wasDarkMode)
-  body.classList.toggle('dark_mode', !wasDarkMode)
-}
-
-document.querySelector('.black_theme').addEventListener('click',darkmode)
